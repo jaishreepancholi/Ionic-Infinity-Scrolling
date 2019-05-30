7099cd75577b63168f719ca3562af35cecd52363
jasihreepancholi <jaishreepancholi30@gmail.com>
2019-05-30 09:31:32 +0530

    Infinity Scrolloing in ionic

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  items = [];
+  numTimesLeft = 5;
+
+  constructor() {
+    this.addMoreItems();
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.addMoreItems();
+      this.numTimesLeft -= 1;
+      event.target.complete();
+    }, 2000);
+  }
+
+  addMoreItems() {
+    for (let i = 0; i < 10; i++)
+      this.items.push(i);
+  }
+}
